0ebe1d9f81426ca2b41ed7447119295bc24285d1
using-system <mnicolescu@gmail.com>
2025-02-04 08:54:01 +0100

    fix istio gateway listeners

--- infra/app-spoke-resources/data.tf
+++ infra/app-spoke-resources/data.tf
@@ -6,7 +6,6 @@ data "azurerm_key_vault" "hub" {
   name                = "cob-hub-infra-we"
   resource_group_name = "cob-hub-infra-we-vault"
 }
-
 data "azurerm_kubernetes_cluster" "cob" {
   name                = var.aks_cluster_name
   resource_group_name = var.aks_resource_group_name
